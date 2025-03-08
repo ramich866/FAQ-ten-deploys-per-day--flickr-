01da561d4f77cb2679f8d669f5afabc479c2dc47
ramich866 <ramichhonor@gmail.com>
2025-03-08 14:42:24 +0200

    first commit

---
+++ article-server/Connection/connection.php
@@ -0,0 +1,16 @@
+<?php
+  header("Access-Control-Allow-Origin:*");
+  header("Access-Control-Allow-Methods: POST");
+  header("Access-Control-Allow-Headers: *");
+  header("Content-Type: application/json");
+
+  $host = "localhost";
+  $user = "root";
+  $pass = "";
+  $db = "FAQtendeploysperdayflickrdb";
+
+  $conn = new mysqli($host, $user, $pass, $db);
+
+  if($conn->connect_error) {
+    die("Connection failed".$conn->connect_error);
+  }
