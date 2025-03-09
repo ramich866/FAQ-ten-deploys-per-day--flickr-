73301578a9e90a28fbee726864880b9d91f5f810
ramich866 <ramichhonor@gmail.com>
2025-03-09 14:11:59 +0200

    Migrations: users table

---
+++ article-server/Database/Migrations/users-table.php
@@ -0,0 +1,11 @@
+<?php
+require_once("../../Connection/connection.php");
+
+$query = "CREATE TABLE users (
+id INT(11) AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL UNIQUE,
+password VARCHAR(255) NOT NULL UNIQUE
+)";
+
+$result = mysqli_query($conn, $query);
