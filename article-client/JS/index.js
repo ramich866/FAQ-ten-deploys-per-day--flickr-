d52e8a47009e18aacf77572e4df589acfc7e7eab
ramich866 <ramichhonor@gmail.com>
2025-03-10 06:25:17 +0200

    add index.js

---
+++ article-client/JS/index.js
@@ -0,0 +1,20 @@
+const username_input = document.getElementById("Username");
+const password_input = document.getElementById("Password");
+const login_button = document.getElementById("Login-Button");
+
+login_button.addEventListener("click", async () => {
+        const form = new FormData();
+
+        form.append("username", username_input.value);
+        form.append("password", password_input.value);
+
+        const response = await axios.post("http://localhost/FAQ-ten-deploys-per-day--flickr/article-server/API/loginUser.php", form);
+
+        if (response.data.status == "success") {
+            window.location.href = "home.html";
+        } else if (response.data.status == "error") {
+            console.log("error");
+        }
+
+    }
+)
