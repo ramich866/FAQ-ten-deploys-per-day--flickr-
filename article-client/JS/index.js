92de9c5cdb8872d5968ac5195387b3fcec87497f
ramich866 <ramichhonor@gmail.com>
2025-03-10 07:08:49 +0200

    add event listener for submit_faq_button

--- article-client/JS/index.js
+++ article-client/JS/index.js
@@ -1,9 +1,14 @@
-// variables
+// user variables
 const email_input = document.getElementById("Email");
 const username_input = document.getElementById("Username");
 const password_input = document.getElementById("Password");
 const login_button = document.getElementById("Login-Button");
 const signup_button = document.getElementById("Signup-Button");
+
+// FAQ variables
+const question_input = document.getElementById("Question");
+const answer_input = document.getElementById("Answer");
+const submit_faq_button = document.getElementById("Submit");
 
 // Login button
 login_button.addEventListener("click", async () => {
@@ -41,3 +46,22 @@ signup_button.addEventListener("click", async () => {
 
 }
 )
+
+submit_faq_button.addEventListener('click', async () => {
+  const form = new FormData();
+
+  form.append("question",question_input.value);
+  form.append("answer", answer_input.value);
+
+  const response = await axios.post(
+      "http://localhost/FAQ-ten-deploys-per-day--flickr/article-server/API/createQuestion.php",
+      form
+  );
+
+  if (response) {
+      answer_input.value = "";
+      question_input.value = "";
+      alert(response.data.message);
+  }
+
+});
