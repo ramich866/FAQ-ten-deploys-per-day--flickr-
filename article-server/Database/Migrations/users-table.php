690591088f94f80ad392521a2b1c1322fed50b50
ramich866 <ramichhonor@gmail.com>
2025-03-10 08:04:01 +0200

    quick fixes

--- article-server/Database/Migrations/users-table.php
+++ article-server/Database/Migrations/users-table.php
@@ -3,7 +3,7 @@ require("../../Connection/connection.php");
 
 $query = "CREATE TABLE users (
 id INT(11) AUTO_INCREMENT PRIMARY KEY,
-firstname VARCHAR(255) NOT NULL,
+username VARCHAR(255) NOT NULL,
 email VARCHAR(255) NOT NULL UNIQUE,
 password VARCHAR(255) NOT NULL UNIQUE
 )";
